12cb5a7bfe1ff102039c86d57ab849147edf1fea
Jjenny-K <kkjjh0129@gmail.com>
2022-04-12 23:40:02 +0900

    Feat: users app 모델 생성

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# AbstractUser 모델 상속
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    date_joined = None
+
+    created_at = models.DateTimeField('가입 날짜', auto_now_add=True)
+    updated_at = models.DateTimeField('수정 날짜', auto_now=True)
